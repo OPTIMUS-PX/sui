5c9ed7fa71f2f6fe5a6f905e4fc3213eee9f999d
William Robertson <williamrobertson@mystenlabs.com>
2023-09-06 14:40:34 -0400

    work

---
+++ sdk/dapp-kit/src/errors/walletErrors.ts
@@ -0,0 +1,22 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * An error that is instantiated when someone attempts to connect to a wallet that isn't registered.
+ */
+export class WalletNotFoundError extends Error {
+	constructor(message: string) {
+		super(message);
+		Object.setPrototypeOf(this, WalletNotFoundError.prototype);
+	}
+}
+
+/**
+ * An error that is instantiated when someone attempts to connect to a wallet that they're already connected to.
+ */
+export class WalletAlreadyConnectedError extends Error {
+	constructor(message: string) {
+		super(message);
+		Object.setPrototypeOf(this, WalletAlreadyConnectedError.prototype);
+	}
+}
--- sdk/dapp-kit/src/utils/walletUtils.ts
+++ sdk/dapp-kit/src/utils/walletUtils.ts
@@ -3,6 +3,9 @@
 
 import type { Wallet, WalletWithSuiFeatures } from '@mysten/wallet-standard';
 import { isWalletWithSuiFeatures } from '@mysten/wallet-standard';
+import type { StorageAdapter } from './storageAdapters.js';
+
+const noSelectedAccountStoragePlaceholder = 'no-selected-account';
 
 export function sortWallets(
 	wallets: readonly Wallet[],
@@ -22,4 +25,49 @@ export function sortWallets(
 		// Wallets in default order:
 		...suiWallets.filter((wallet) => !preferredWallets.includes(wallet.name)),
 	];
+}
+
+export async function setMostRecentWalletConnectionInfo({
+	storageAdapter,
+	storageKey,
+	walletName,
+	accountAddress,
+}: {
+	storageAdapter: StorageAdapter;
+	storageKey: string;
+	walletName: string;
+	accountAddress?: string;
+}) {
+	try {
+		await storageAdapter.set(
+			storageKey,
+			`${walletName}-${accountAddress ?? 'noSelectedAccountStoragePlaceholder'}`,
+		);
+	} catch (error) {
+		// We'll skip error handling here and just report the error to the console since persisting connection
+		// info isn't essential functionality and storage adapters can be plugged in by the consumer.
+		console.error('[dApp-kit] Error: Failed to save wallet connection info to storage.');
+	}
+}
+
+export async function getMostRecentWalletConnectionInfo(
+	storageAdapter: StorageAdapter,
+	storageKey: string,
+) {
+	try {
+		const lastWalletConnectionInfo = await storageAdapter.get(storageKey);
+		if (lastWalletConnectionInfo) {
+			const [walletName, accountAddress] = lastWalletConnectionInfo.split('-');
+			return {
+				walletName,
+				accountAddress:
+					accountAddress === noSelectedAccountStoragePlaceholder ? undefined : accountAddress,
+			};
+		}
+	} catch (error) {
+		// We'll skip error handling here and just report the error to the console since retrieving connection
+		// info isn't essential functionality and storage adapters can be plugged in by the consumer.
+		console.error('[dApp-kit] Error: Failed to retrieve wallet connection info from storage.');
+	}
+	return {};
 }
