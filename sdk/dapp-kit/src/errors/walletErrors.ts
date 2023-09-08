50e7935d54f20a2a094ae2fc3d17c3d828076672
William Robertson <williamrobertson@mystenlabs.com>
2023-09-08 15:18:03 -0400

    fix

--- sdk/dapp-kit/src/errors/walletErrors.ts
+++ sdk/dapp-kit/src/errors/walletErrors.ts
@@ -14,19 +14,9 @@ export class WalletNotFoundError extends Error {
 /**
  * An error that is instantiated when someone attempts to connect to a wallet that they're already connected to.
  */
-export class WalletAlreadyConnectedError extends Error {
-	constructor(message: string) {
-		super(message);
-		Object.setPrototypeOf(this, WalletAlreadyConnectedError.prototype);
-	}
-}
+export class WalletAlreadyConnectedError extends Error {}
 
 /**
  * An error that is instantiated when someone attempts to perform an action that requires an active wallet connection.
  */
-export class WalletNotConnectedError extends Error {
-	constructor(message: string) {
-		super(message);
-		Object.setPrototypeOf(this, WalletNotConnectedError.prototype);
-	}
-}
+export class WalletNotConnectedError extends Error {}
